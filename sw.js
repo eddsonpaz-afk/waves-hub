ba7cbbc42e3652ad67dd5802b4b335cce785f5f0
eddsonpaz-afk <eddsonpaz@gmail.com>
2026-07-01 08:32:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("catalogo-waves-plus-v4").then(cache => {
-      return cache.addAll([
-        "./",
-        "index.html",
-        "css/app.css",
-        "js/app.js",
-        "data/produtos.json",
-        "data/categorias.json",
-        "manifest.json"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+// Service worker desativado para evitar cache antigo.
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))))})
